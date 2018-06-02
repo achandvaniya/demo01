4d1bc28d71cd8f587771e60f09357d603d3a147d
Ajay <ajay.cse@live.com>
2018-06-02 23:51:39 +0530

    starting work and fix models

--- Homework/homework/admin.py
+++ Homework/homework/admin.py
@@ -3,4 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
+from .models import City, Cleaner, CleanerCityMap
 # Register your models here.
+
+admin.site.register(City)
+admin.site.register(Cleaner)
+admin.site.register(CleanerCityMap)
--- Homework/homework/models.py
+++ Homework/homework/models.py
@@ -10,33 +10,41 @@ class BaseModel(models.Model):
         abstract = True
 
 class City(BaseModel):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, unique=True)
     
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = "Cities"
 
 class Customer(BaseModel):
-    fist_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=18, null=True, blank=True)
     # I have make this field as string so that user can store no like +9112.. , 0921.... etc
       
     def __str__(self):
-        return self.fist_name + " " + self.last_name
+        return self.first_name + " " + self.last_name
 
 class Cleaner(BaseModel):
     """docstring for Cleaner"""
-    fist_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    quality_score = models.DecimalField(max_digits=2, decimal_places=2)
+    quality_score = models.DecimalField(max_digits=10, decimal_places=2)
   
     def __str__(self):
-        return self.fist_name + " " + self.last_name
+        return self.first_name + " " + self.last_name
 
 class CleanerCityMap(BaseModel):
     Cleaner = models.ForeignKey(Cleaner)
     City = models.ForeignKey(City)
+
+    class Meta:
+        unique_together = ('Cleaner', 'City')
+
+    def __str__(self):
+        return "{}_{} - {}".format(self.Cleaner.first_name, self.Cleaner.last_name ,self.City.name)
     
 
 class Booking(BaseModel):
