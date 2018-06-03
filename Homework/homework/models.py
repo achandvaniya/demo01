ca578feaadab6d44eda37859377bf9ad47b87083
Ajay <ajay.cse@live.com>
2018-06-03 12:12:32 +0530

    change phone number field and start work

--- Homework/homework/models.py
+++ Homework/homework/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class BaseModel(models.Model):
     created_date = models.DateTimeField(auto_now_add=True, null=True, blank=True)
@@ -21,8 +22,12 @@ class City(BaseModel):
 class Customer(BaseModel):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=18, null=True, blank=True)
-    # I have make this field as string so that user can store no like +9112.. , 0921.... etc
+    phone_number = models.IntegerField(
+                        validators=[
+                            MaxValueValidator(10),
+                            MinValueValidator(10)
+                        ]
+                     )
       
     def __str__(self):
         return self.first_name + " " + self.last_name
