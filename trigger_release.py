035fb8bfaac6db024136cf30f79be08a500930ce
Ajay Chandwaniya <achandwa@greenpeace.org>
2024-04-07 12:35:49 +0530

    test

--- trigger_release.py
+++ trigger_release.py
@@ -1,1 +1,26 @@
-print("Release will be trigger")
+import requests
+import json
+import os
+
+session = requests.Session()
+
+
+session.auth = (os.environ['username'], os.environ['token'])
+
+url = "https://dev.azure.com/ajaycs1991/ajaycs1991/_apis/pipelines/1/runs?api-version=7.1-preview.1"
+
+auth = session.post(url)
+
+payload = json.dumps({
+  "templateParameters": {
+    "name": "ac"
+  }
+})
+
+headers = {
+  'Content-Type': 'application/json',
+}
+
+response = session.post(url, headers=headers, data=payload)
+
+print(response.text)
