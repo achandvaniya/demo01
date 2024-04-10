baec9e1898983041fd8816c31dcdf04e16b0faa4
Ajay Chandwaniya <achandwa@greenpeace.org>
2024-04-10 19:40:58 +0530

    test

--- trigger_release.py
+++ trigger_release.py
@@ -5,8 +5,7 @@ import sys
 print("-------------------------")
 print("-->>> ", sys.argv) 
 print("-->>> ", sys.argv[0])
-print("-->>> ", sys.argv[1])
-print("-->>> ", sys.argv[2])
+print("-->>> ", sys.argv[1:])
 
 # session = requests.Session()
 
