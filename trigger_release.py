e0b479b109110bdaaa1791706bcc5203c43794a9
Ajay Chandwaniya <achandwa@greenpeace.org>
2024-04-10 23:05:50 +0530

    test

--- trigger_release.py
+++ trigger_release.py
@@ -1,34 +1,37 @@
 # import requests
 import json
-import os
+import os, time
 import sys, ast
 
 pipeline_configs = os.getenv('CONFIG')
 print (pipeline_configs)
 pipeline_configs = ast.literal_eval(pipeline_configs)
-pipeline_configs_deatails = []
-for config in pipeline_configs:
-    for key ,value in config.items():
-      defination_id = key
-      artifect_id = value
-      print (defination_id, artifect_id)
-      pipeline_configs_deatails.append({"defination_id": defination_id, "artifect_id": artifect_id })
 
-print("--------")
-print(pipeline_configs_deatails)
+print(pipeline_configs)
 
-release_ids = []
+# pipeline_configs_deatails = []
+# for config in pipeline_configs:
+#     for key ,value in config.items():
+#       defination_id = key
+#       artifect_id = value
+#       print (defination_id, artifect_id)
+#       pipeline_configs_deatails.append({"defination_id": defination_id, "artifect_id": artifect_id })
 
-def trigger_release_pipeline(defination_id, artifect_id):
-  #  return release_id
-  return 11
+# print("--------")
+# print(pipeline_configs_deatails)
 
-for pipeline_config in pipeline_configs_deatails:
-  defination_id, artifect_id = pipeline_config['defination_id'], pipeline_config['artifect_id']
-  release_id = trigger_release_pipeline(defination_id, artifect_id)
-  release_ids.append(release_id)
+# release_ids = []
 
-print (release_ids)
+# def trigger_release_pipeline(defination_id, artifect_id):
+#   #  return release_id
+#   return 11
+
+# for pipeline_config in pipeline_configs_deatails:
+#   defination_id, artifect_id = pipeline_config['defination_id'], pipeline_config['artifect_id']
+#   release_id = trigger_release_pipeline(defination_id, artifect_id)
+#   release_ids.append(release_id)
+
+# print (release_ids)
 
 
 # session = requests.Session()
@@ -63,7 +66,7 @@ print (release_ids)
 # release_id = 2323
 # auth = session.post(url)
 
-# def get_release_status():
+# def get_release_status(release_id):
 #     url = f"https://vsrm.dev.azure.com/AmericanAirlines/AirportStaffManagement/_apis/release/releases/{release_id}?api-version=7.1-preview.8"
 #     response = session.get(url, headers=headers)
 #     return response
@@ -72,11 +75,21 @@ print (release_ids)
 # retry = 5
 # status = ""
 
-# while (status not in ("success", "rejected")) and retry >= 0:
-#     response = get_release_status()
-#     print(response.text)
+# while len(release_ids) > 0:
+#   for release_id in release_ids:
+#     response = get_release_status(release_id)
 #     json_res = json.loads(response.text)
 #     status = json_res["environments"][0]["status"]
-#     retry = retry - 1
-#     time.sleep(120)
 #     output_res = {"stag name":json_res["environments"][0]["name"],"status": status,"release_id": json_res["environments"][0]["releaseId"]}
+#   if status in ["successed", "rejected"]:
+#     index_of_release_id = release_ids.index(release_id)
+#     del release_ids[index_of_release_id]
+#   time.sleep(20)
+
+
+
+# while len(release_ids) > 0:
+#   for release_id in release_ids:
+#     print(release_id)
+#     index_of_release_id = release_ids.index(release_id)
+#     del release_ids[index_of_release_id]
